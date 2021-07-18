d4f91d25d3ca7f507156029c397d8f82d810227c
kawai9126 <iroha.9126@gmail.com>
2021-07-18 12:18:26 +0000

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :end_users
+
+  devise_for :admins,controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+
+  devise_for :end_users,controllers: {
+  sessions:      'end_users/sessions',
+  passwords:     'end_users/passwords',
+  registrations: 'end_users/registrations'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'homes#top'
+
 end
+
+
+
