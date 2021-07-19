dad9cb6c6f90aa13da39e325db7248773afcd8c2
kawai9126 <iroha.9126@gmail.com>
2021-07-19 14:23:28 +0000

    add admin,route,genre,item

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,44 @@
 class Public::ItemsController < ApplicationController
     
     def index
+    @items = Item.all
+    #@items = Item.page(params[:page])
+    end
+    def new
+        @item = Item.new
+    end
+    def edit
+        @item = Item.find(params[:id])
+    end
+    def show
+        @item = Item.find(params[:id])
+        @cart_item = CartItem.new
+        #@genre = Genre.find(params[:id])
+    end
+    def create
+        @item = Item.new(item_params)
+        @item.save
+        redirect_to public_item_path(@item)
+    end
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        redirect_to public_item_path(@item)
     end
     
-    
+    private
+    def item_params
+        params.require(:item).permit(:image, :name, :contents, :genre_id, :non_price, :is_sale)
+    end
+
 end
+
+
+# def index
+#         @items = Item.page(params[:page]).reverse_order
+#         @totals = Item.all
+#     end
+#     def show
+#         @item = Item.find(params[:id])
+#         @cart_item = CartItem.new
+#     end
--- config/routes.rb
+++ config/routes.rb
@@ -15,20 +15,36 @@ devise_for :end_users, controllers: {
 
   root to: 'homes#top'
   
+#ユーザー側  
   namespace :public do
-    resources :items,only: [:index,:show]
-  
-  
+    resources :items,only: [:index,:show,:edit,:new,:create,:update]
     resources :end_users,only: [:show,:edit,:update] do
       member do
         get 'user_exit'
         patch 'update_exit'
       end
      end
+     resources :orders,only: [:index,:create,:show,:new] do
+     collection do
+      get 'finish'
+      post 'check'
+     end
     end
-
-
+   resources :shipping_addresses,only: [:index,:destroy,:update,:edit,:create]
+    resources :cart_items,only: [:index,:destroy,:update,:create] do
+     collection do
+      delete 'destroy_all'
+     end
+   end
+  end
+  
+# 管理側
+  namespace :admin do
+    resources :genres,only: [:index,:edit,:update,:create]
+    resources :end_users,only: [:index,:edit,:show,:update]
+    resources :items,only: [:index,:edit,:show,:create,:update,:new]
+    resources :orders,only: [:index,:show,:update]
+    resources :order_items,only: [:update]
+    resources :adimin,only: [:index,:create,:destroy]
+  end
 end
-
-
-
